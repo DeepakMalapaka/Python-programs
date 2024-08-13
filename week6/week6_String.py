1674a8954a02f4d890c0f05ce958be6860d8f5b9
DeepakMalapaka <154827923+DeepakMalapaka@users.noreply.github.com>
2024-08-13 12:32:39 +0530

    Add files via upload

---
+++ week6/week6_String.py
@@ -0,0 +1,23 @@
+#concatenation 
+"""s1="Malapaka"
+s2="Deepak"
+print(s1+" "+s2)"""
+
+#Repition
+"""s1="Hello "
+s2="World"
+print(s1*3+s2,end="!")
+print("")"""
+
+#Iterating through a string
+"""for i in "Hello World!":
+	print(i,end=" ")
+print("")"""
+
+#Membership operator
+"""str1="HELLO"
+print('E' in str1)
+print('z' not in str1)"""
+
+#Slicing
+
