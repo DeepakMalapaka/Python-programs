88da6d2ffe7958390289bbc98b002010c18de900
DeepakMalapaka <154827923+DeepakMalapaka@users.noreply.github.com>
2024-08-13 09:58:05 +0530

    Create tuple.py

---
+++ week6/tuple.py
@@ -0,0 +1,58 @@
+#Tuple concatenation
+"""t1=(1,2,3)
+t2=(4,5,6)
+print(t1+t2)"""
+
+#Repetition
+"""t=(1,2,3) 
+print(t*3)"""
+
+#Index 
+"""t=(1,2,3,4,5)
+print(t[1])
+print(t[2])"""
+
+#Membership operator
+"""t=(1,2,3)
+print(1 in t)
+print(4 not in t)"""
+
+#Iteration 
+"""for i in (1,2,3) :
+	print(i)"""
+
+#Slicing 
+"""t=('a','b','c','d', 'e','f','g')
+print(t[1])
+print(t[1:3])
+print(t[1:-1])
+print(t[:3])
+print(t[2:])
+print(t[:-1])
+print(t[1::2])
+print(t[::-2])"""
+
+#Tuple length
+"""t=(1,2,3,4,5,6,7,8,9,0)
+print(t[4])
+print(t[1])"""
+
+#Tuple length
+"""t=(1,2,3,4,5,6,7,8,9,0)
+print(len(t))"""
+
+#count
+"""t=(1,2,3,4,4,5,6)
+print(t.count(4))"""
+
+#copy
+"""t1=(1,2,3)
+t2=t1
+print(t1)
+print(t2)"""
+
+#Zip method 
+"""t1=(1,2,3)
+t2=(4,5,6,7,8,9)
+t3=list(zip(t1,t2))
+print(t3)"""
